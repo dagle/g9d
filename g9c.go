8900a4ffe1d0bdc7648d353f86d233dcf3fc0562
Per Odlund <per.odlund@gmail.com>
2014-04-08 20:13:05 +0200

    Add support for relative paths and formating.

--- g9c.go
+++ g9c.go
@@ -1,25 +1,80 @@
 package main
 
 import (
-	"os"
+	"flag"
 	"fmt"
+	"os"
+	"os/user"
 	"path"
+	"strings"
 )
 
-func main() {
-	pwd,err := os.Getwd()
+func absolute(pwd, path string) string {
+	os.Chdir(path)
+	newPath, err := os.Getwd()
 	if err != nil {
 		panic("Could not get the current working directory")
 	}
-	for _,arg := range os.Args[1:] {
-		dir := path.Dir(arg)
-		base := path.Base(arg)
-		os.Chdir(dir)
-		newPwd,err := os.Getwd()
-		if err != nil {
-			panic("Could not get the current working directory")
+	os.Chdir(pwd)
+	return newPath
+}
+
+func updir(str string) (string, string) {
+	dirs := strings.Split(str, "\n")
+	i := len(dirs)
+	if i == 1 {
+		return "/", "/"
+	}
+	return strings.Join(dirs[:(i-2)], "/"), dirs[i-1]
+}
+
+func toHome(pwd, dir string) string {
+	var str string
+	usr, err := user.Current()
+	if err != nil {
+		panic("Could not get the users homedir")
+	}
+	home := usr.HomeDir
+	os.Chdir(dir)
+	npwd := os.Getenv("PWD")
+	for {
+		var s string
+		if npwd == "/" {
+			os.Chdir(pwd)
+			return absolute(pwd, dir)
 		}
-		fmt.Printf("%s/%s\n", newPwd, base)
-		os.Chdir(pwd)
+		if npwd == home {
+			if str == "" {
+				return dir
+			} else {
+				return str + "/" + dir
+			}
+		}
+		npwd, s = updir(npwd)
+		str = s + "/" + str
+	}
+}
+
+var abs = flag.Bool("a", false, "Absolute path")
+
+func main() {
+	flag.Parse()
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic("Could not get the current working directory")
+	}
+	for _, arg := range flag.Args()[:] {
+		base := path.Base(arg)
+		dir := path.Dir(arg)
+		if *abs {
+			newPath := absolute(pwd, dir)
+			fmt.Printf("%s/%s\n", newPath, base)
+		} else {
+			if strings.HasPrefix(dir, "/") {
+				fmt.Printf("%s/%s\n", dir, base)
+			} else {
+				fmt.Printf("%s/%s\n", toHome(pwd, dir), base)
+			}
+		}
 	}
 }
